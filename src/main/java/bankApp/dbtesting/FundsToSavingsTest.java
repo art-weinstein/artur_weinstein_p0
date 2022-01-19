3496e1f2a8083c72b1c17f638b7bf4cb20139647
Art Weinstein <artur.weinstein@gmail.com>
2022-01-18 18:57:35 -0800

    create a funds to savings test

--- src/main/java/bankApp/dbtesting/FundsToSavingsTest.java
+++ src/main/java/bankApp/dbtesting/FundsToSavingsTest.java
@@ -1,15 +1,16 @@
 package bankApp.dbtesting;
-import bankApp.repositories.AccountRepoDBImpl;
+
 import bankApp.repositories.AccountRepo;
+import bankApp.repositories.AccountRepoDBImpl;
 
-import static bankApp.models.Account.transferToChecking;
+import static bankApp.models.Account.transferToSavings;
 
-public class FundsToCheckingTest {
+public class FundsToSavingsTest {
     public static void main(String[] args) {
         AccountRepo ar = new AccountRepoDBImpl();
         String username = "test6";
         String password = "test6";
         ar.login(username, password);
-        transferToChecking();
+        transferToSavings();
     }
 }
